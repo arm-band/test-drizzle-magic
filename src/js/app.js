8ab8783409f586cc3473ffe06f08d555917f863e
arm-band <bre4tn.let@gmail.com>
2021-12-16 23:48:12 +0900

    first commit. ナビゲーションバーの折りたたみ(collapse)挙動をカスタマイズ。 Bootstrap4 のcollapseイベントにAPIで干渉してデフォルトの挙動を強制停止。その上で .navbar-toggler と #navbarList にクラスの付与/削除を自前で行い、 css transition でアニメーションを実装。

---
+++ src/js/app.js
@@ -0,0 +1,23 @@
+$(() => {
+    if($('#navbarList').length > 0 && $('#navbar .navbar-toggler').length > 0) {
+        // navbar にメニューとハンバーガーアイコンが存在する場合
+        const $navbarList = $('#navbarList');
+        const $navbarToggler = $('#navbar .navbar-toggler');
+        $navbarList.on('show.bs.collapse', function () {
+            // 展開時のデフォルトの挙動を潰して Collapse の結果のクラスの付与・削除のみ実施
+//            e.preventDefault();
+//            e.stopPropagation();
+            $navbarToggler.removeClass('collapsed');
+            $navbarList.addClass('show');
+            return false;
+        })
+        $navbarList.on('hide.bs.collapse', function () {
+            // 格納時のデフォルトの挙動を潰して Collapse の結果のクラスの付与・削除のみ実施
+//            e.preventDefault();
+//            e.stopPropagation();
+            $navbarToggler.addClass('collapsed');
+            $navbarList.removeClass('show');
+            return false;
+        });
+    }
+});
